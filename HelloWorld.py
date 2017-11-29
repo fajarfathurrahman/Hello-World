6572d8d07964c55301767785f80623c67fe132c2
fajarfathurrahman <fajarfathurrahman@users.noreply.github.com>
2017-11-29 11:06:15 +0700

    Hello World

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,13 @@
+## Simple Hello World App 20171129 ##
+
+mynumber = 3.14
+print(mynumber)
+myfloat = float(3.14)
+print(myfloat)
+
+mystring = "hello"
+mystring2 = 'hello2'
+print(mystring)
+print(mystring2)
+
 print("Hello World!")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Hello World!
+# created with Python
